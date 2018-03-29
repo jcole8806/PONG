a454dcbb5ad3501c5938d6fe201d3085ca6dfdd5
colejr1 <colejr1@itsd-54htzw1-wl.dom1.jhuapl.edu>
2018-03-29 15:58:01 -0400

    Set up basic tasks to start the program

--- src/Pong.java
+++ src/Pong.java
@@ -1,9 +1,25 @@
+import javax.swing.JFrame;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class Pong {
-
+public class Pong{
+	static JFrame pongFrame;
+	static Timer t = new Timer();
+	
+	public Pong() {
+		pongFrame = new JFrame("PONG");
+		pongFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		pongFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		pongFrame.setVisible(true);
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new Pong();
+		t.schedule(new TimerTask() {
+			public void run() {
+				pongFrame.repaint();
+			}
+		}, 0, 10);
 	}
 
 }
