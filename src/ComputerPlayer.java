22200d0ffe5ae4d63f7434c77e5bd3acb3c0f94b
tkhan0770 <tkhan0770@CHHB2ZLDXV1.inst.hcpss.org>
2018-04-19 13:41:27 -0400

    A way better strategy. Play against it and try to win.

--- src/ComputerPlayer.java
+++ src/ComputerPlayer.java
@@ -23,16 +23,21 @@ public class ComputerPlayer extends Player implements LocationListener{
 			if(yChanged > 0 && this.paddle.yPos + this.paddle.size < Pong.screenSize.height - 75)
 				this.paddle.yPos+=2*dir;
 			else if(yChanged < 0 && this.paddle.yPos > 0)
+				
 				this.paddle.yPos-=2*dir;
 			
-			
-		}
-		
-		if(event.getY() > this.getPaddle().yPos){
-			while(this.getPaddle().yPos != event.getY()){
-				this.paddle.yPos++;
+			if(event.getY() > this.getPaddle().yPos){
+				while(this.getPaddle().yPos != event.getY()){
+					this.paddle.yPos++;
+				}
+			}else if(event.getY() < this.getPaddle().yPos){
+				while(this.getPaddle().yPos != event.getY()){
+					this.paddle.yPos--;
+				}
 			}
 		}
+		
+		
 		
 		this.previousLocation = new Point(event.getX(),event.getY());
 	}
